cd48b087e98fb0e27f5587f3282e35eea3637eaf
Daniel Chuks <danieluchechukwuchuks1@gmail.com>
2023-02-11 03:56:18 +0100

    custom hooks with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import Form from './components /Form';
-import MovieList from './components /MovieList';
-import Search from './components /Seacrch';
-
-
+// import Form from './components /Form';
+// import MovieList from './components /MovieList';
+// import Search from './components /Seacrch';
 import './App.css';
+import AlbumList from './components /album/AlbumList';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      {/* <Form />
       <MovieList />
-      <Search />
+      <Search /> */}
+      <AlbumList />
     </div>
   );
 }
---
+++ src/Hooks/UseFetch.ts
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export interface UseFetchProps<T> {
+    error: boolean;
+    loading: boolean;
+    data: T;
+}
+
+export default function UseFetch<T>(url: string): UseFetchProps<T[]> {
+    const [data, setData] = useState<T[]>([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+
+    /**
+     * creating a limit to the data we are fetching from the api
+     */
+    const urlWithLimit = `${url}?limit=5`;
+
+    useEffect(() => {
+        setTimeout(() => {
+            const fetchData = async () => {
+                setLoading(true);
+                try {
+                    const response = await axios.get(urlWithLimit);
+                    setData(response.data);
+                    setError(false);
+                    setLoading(false);
+    
+                    if (!response) {
+                        setLoading(false);
+                        setError(true);
+                        throw new Error('there was an error fetching');
+                    }
+                } catch (err) {
+                    setError(true);
+                } finally {
+                    setLoading(false);
+                }
+            };
+    
+            fetchData();
+        }, 1000)
+       
+    }, [urlWithLimit]);
+
+    return {
+        data,
+        loading,
+        error,
+    };
+}
